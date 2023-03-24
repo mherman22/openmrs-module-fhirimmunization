8975e17b2fd00e11acb496040ef696de2c96b5da
herman <hermanmuhereza22@gmail.com>
2023-03-24 11:27:38 +0300

    add some java docs to explain some fields

--- api/src/main/java/org/openmrs/module/procedure/Procedure.java
+++ api/src/main/java/org/openmrs/module/procedure/Procedure.java
@@ -11,34 +11,57 @@ package org.openmrs.module.procedure;
 
 import org.openmrs.BaseOpenmrsData;
 import org.openmrs.Patient;
-import org.openmrs.User;
+
 import java.io.Serializable;
 
+/**
+ * This resource is used to record the details of current and historical procedures performed on or for a patient.
+ * A procedure is an activity that is performed on, with, or for a patient as part of the provision of care.
+ * Examples include surgical procedures, diagnostic procedures, endoscopic procedures, biopsies, counseling, physiotherapy,
+ * personal support services, adult day care services, non-emergency transportation, home modification, exercise, etc.
+ * Procedures may be performed by a healthcare professional, a service provider, a friend or relative or in some cases by the patient themselves.
+ */
 public class Procedure extends BaseOpenmrsData implements Serializable {
 	
 	private Integer procedureId;
 	
+	/**
+	 *
+	 * Generally, this will be the in-progress or completed state.
+	 * @see <a href="https://www.hl7.org/fhir/procedure-definitions.html#Procedure.status">Procedure.status</a>
+	 */
 	private String status;
 	
+	/**
+	 * reason for the current status
+	 * @see <a href="https://www.hl7.org/fhir/procedure-definitions.html#Procedure.statusReason">Procedure.statusReason</a>
+	 */
 	private String statusReason;
 	
 	private String category;
 	
 	private String procedureCode;
 	
-	private User performerOfTheProcedure;
+	private String performerOfTheProcedure;
 	
 	private String bodySite;
 	
+	/**
+	 * The outcome of the procedure - did it resolve the reasons for the
+	 * procedure being performed?
+	 */
 	private String outcome;
 	
+	/**
+	 * The person, animal or group on which the procedure was performed.
+	 */
 	private Patient subject;
 	
 	public Procedure() {
 	}
 	
 	public Procedure(Integer procedureId, String status, String statusReason, String category, String procedureCode,
-	    User performerOfTheProcedure, String bodySite, String outcome, Patient subject) {
+	    String performerOfTheProcedure, String bodySite, String outcome, Patient subject) {
 		this.procedureId = procedureId;
 		this.status = status;
 		this.statusReason = statusReason;
@@ -90,11 +113,11 @@ public class Procedure extends BaseOpenmrsData implements Serializable {
 		this.procedureCode = procedureCode;
 	}
 	
-	public User getPerformerOfTheProcedure() {
+	public String getPerformerOfTheProcedure() {
 		return performerOfTheProcedure;
 	}
 	
-	public void setPerformerOfTheProcedure(User performerOfTheProcedure) {
+	public void setPerformerOfTheProcedure(String performerOfTheProcedure) {
 		this.performerOfTheProcedure = performerOfTheProcedure;
 	}
 	
